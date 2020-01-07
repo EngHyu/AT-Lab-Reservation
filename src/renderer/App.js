7f202eb5f26219ea62e13410c1b7da4500604bac
김동호 <gimdongho@gimdonghoui-MacBookPro.local>
2020-01-07 23:18:05 +0900

    project setting: electron-webpack-quick-start + react + @babel/preset-react

---
+++ src/renderer/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        electron-webpack-quick-start + react + @babel/preset-react
+      </div>
+    );
+  }
+}
